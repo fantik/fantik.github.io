425c27974790450cd979364d7b37be18358153f9
Demchenko Denys <denib90185@gmail.com>
2017-10-27 13:32:22 +0300

    new websites(no edit)

---
+++ Genesis/js/common.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+	$(".up, .animation_4").animated("fadeInDown", "fadeOutUp");
+	$(".down, .animation_5").animated("fadeInUp", "fadeOutDown");
+
+	$(".animation_1").animated("zoomIn", "zoomOut");
+	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
+	$(".animation_3").animated("fadeInRight", "fadeOutRight");
+
+		function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+		$("input, select, textarea").jqBootstrapValidation();
+
+		$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});	
+	
+});
