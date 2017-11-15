b1eee83fce7d5716d23a6caec4193f38ea45249b
fantik <denib90185@gmail.com>
2017-11-15 15:32:44 +0200

    fin edit

--- Mlove/js/common.js
+++ Mlove/js/common.js
@@ -38,4 +38,19 @@ $(document).ready(function() {
 	$(window).resize(function() {
 		heightDetect();
 	});
+	PopUpHide();
+	PopUpHide2();
 });
+
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+function PopUpShow2(){
+    $("#popup2").show();
+}
+function PopUpHide2(){
+    $("#popup2").hide();
+}
